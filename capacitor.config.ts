16c732858fe36ef428cf5bb3719e97311194f104
JennerCruz <pamecoaxaca.telcel@gmail.com>
2026-09-20 14:53:18 -0600

    Add Android APK release

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.notebox.app',
+  appName: 'note.boox',
+  webDir: 'dist'
+};
+
+export default config;
